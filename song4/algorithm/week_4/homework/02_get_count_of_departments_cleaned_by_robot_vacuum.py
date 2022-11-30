6cac2b4209ee8a881b10c5aec1b18c5cd9de4d6a
JiHoon Song <jihoonsong@JiHoonui-MacBookPro.local>
2022-11-30 17:51:05 +0900

    알고리즘 4주차 2번 숙제

---
+++ song4/algorithm/week_4/homework/02_get_count_of_departments_cleaned_by_robot_vacuum.py
@@ -0,0 +1,105 @@
+current_r, current_c, current_d = 7, 4, 0
+current_room_map = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 1, 1, 1, 1, 0, 1],
+    [1, 0, 0, 1, 1, 0, 0, 0, 0, 1],
+    [1, 0, 1, 1, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 1, 0, 1],
+    [1, 0, 0, 0, 0, 0, 1, 1, 0, 1],
+    [1, 0, 0, 0, 0, 0, 1, 1, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+]
+
+
+# 1. 현재 위치를 청소한다.
+# 2. 현재 위치에서 현재 방향을 기준으로 왼쪽방향부터 차례대로 탐색을 진행한다.
+#     a. 왼쪽 방향에 아직 청소하지 않은 공간이 존재한다면, 그 방향으로 회전한 다음 한 칸을 전진하고 1번부터 진행한다.
+#     b. 왼쪽 방향에 청소할 공간이 없다면, 그 방향으로 회전하고 2번으로 돌아간다.
+#     c. 네 방향 모두 청소가 이미 되어있거나 벽인 경우에는, 
+#          바라보는 방향을 유지한 채로 한 칸 후진을 하고 2번으로   돌아간다.
+#     d. 네 방향 모두 청소가 이미 되어있거나 벽이면서, 뒤쪽 방향이 벽이라 후진도 할 수 없는 경우에는 작동을 멈춘다.
+# 로봇 청소기는 이미 청소되어있는 칸을 또 청소하지 않으며, 벽을 통과할 수 없다.
+def get_count_of_departments_cleaned_by_robot_vacuum(r, c, d, room_map):
+    CLEAN = 2
+    clean_count =1
+    robot = [r,c,d]
+    rotation = { # (left r, left c, look, back r, back c)
+        0: [0, -1, 3, 1, 0],  # North -> West
+        1: [-1, 0, 0, 0, -1], # East  -> North
+        2: [0, 1, 1, -1, 0],  # South -> East
+        3: [1, 0, 2, 0, 1]    # West  -> South
+    }
+
+    while 1:
+        room_map[robot[0]][robot[1]] = CLEAN
+        count =0
+        while count < 4:
+            left_check = [rotation[robot[2]][0]+robot[0], rotation[robot[2]][1]+robot[1], robot[2]]
+            left_state = room_map[left_check[0]][left_check[1]]
+            if left_state == 0:
+                robot[0] += rotation[robot[2]][0]
+                robot[1] += rotation[robot[2]][1]
+                robot[2] = rotation[robot[2]][2]
+                clean_count+=1
+                break
+            else:
+                robot[2] = rotation[robot[2]][2]
+                count+=1 # 1 West, 2 South, 3 East, 4 North
+            
+        if count == 4:
+            if room_map[rotation[robot[2]][3]+robot[0]][rotation[robot[2]][4]+robot[1]] != 1:
+                robot[0] += rotation[robot[2]][3]
+                robot[1] += rotation[robot[2]][4]
+            else:
+                return clean_count
+
+    return
+
+
+# 57 가 출력되어야 합니다!
+print(get_count_of_departments_cleaned_by_robot_vacuum(current_r, current_c, current_d, current_room_map))
+current_room_map2 = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 1, 1, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 1, 1, 1, 1, 0, 1],
+    [1, 0, 0, 1, 1, 0, 0, 0, 0, 1],
+    [1, 0, 1, 1, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 1, 0, 1],
+    [1, 0, 0, 0, 0, 0, 1, 1, 0, 1],
+    [1, 0, 0, 0, 0, 0, 1, 1, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+]
+print("정답 = 29 / 현재 풀이 값 = ", get_count_of_departments_cleaned_by_robot_vacuum(6,3,1,current_room_map2))
+current_room_map3 = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 1, 1, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 1, 1, 1, 1, 0, 1],
+    [1, 0, 0, 1, 1, 0, 0, 0, 0, 1],
+    [1, 0, 1, 1, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 1, 0, 1],
+    [1, 0, 0, 0, 0, 0, 1, 1, 0, 1],
+    [1, 0, 0, 1, 0, 0, 1, 1, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+]
+print("정답 = 33 / 현재 풀이 값 = ", get_count_of_departments_cleaned_by_robot_vacuum(7,4,1,current_room_map3))
+current_room_map4 = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 1, 1, 0, 0, 0, 0, 0, 1],
+    [1, 0, 1, 0, 1, 1, 1, 1, 0, 1],
+    [1, 0, 0, 1, 1, 0, 0, 0, 0, 1],
+    [1, 0, 1, 1, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 1, 0, 1],
+    [1, 0, 0, 0, 0, 0, 1, 1, 0, 1],
+    [1, 0, 0, 1, 0, 0, 1, 1, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+]
+print("정답 = 25 / 현재 풀이 값 = ", get_count_of_departments_cleaned_by_robot_vacuum(6,2,0,current_room_map4))
