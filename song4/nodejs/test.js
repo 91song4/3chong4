f08eaa1b8c5ccf77aeb826f1e651715f6f88ae9e
91song4 <91song4@daum.net>
2022-12-12 11:25:57 +0900

    main 즉시실행함수로 만들어보기

--- song4/nodejs/test.js
+++ song4/nodejs/test.js
@@ -1,12 +1,14 @@
-const resolvePromise = new Promise((resolve, rejent) => {
-    setTimeout(() => {
-        console.log('First');
-        resolve('Resolve!');
-    }, 1000);
-});
+const main = async function() {
+    const resolvePromise = new Promise((resolve, rejent) => {
+        setTimeout(() => {
+            console.log('First');
+            resolve('Resolve!');
+        }, 1000);
+    });
 
-resolvePromise.then((data) => {
-    console.log('Middle');
-    console.log('Last');
-    console.log(data);
-})
+    resolvePromise.then((data) => {
+        console.log('Middle');
+        console.log('Last');
+        console.log(data);
+    })
+}();
