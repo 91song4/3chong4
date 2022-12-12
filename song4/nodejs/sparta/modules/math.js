4bd49e5692285ce44c59453a77b383fdaf22b528
91song4 <91song4@daum.net>
2022-12-12 23:11:35 +0900

    module을 사용하는 방법2

--- song4/nodejs/sparta/modules/math.js
+++ song4/nodejs/sparta/modules/math.js
@@ -1,5 +1,5 @@
-function add(a, b) {
+exports.add = function (a, b) {
     return a + b;
 }
 
-module.exports = add;
+// module.exports = add;
--- song4/nodejs/sparta/modules/run.js
+++ song4/nodejs/sparta/modules/run.js
@@ -1,2 +1,3 @@
-const sum = require('./math.js');
-console.log(sum(10, 20));
+const {add} = require('./math.js'); // 객체 구조 분해 할당 {add}
+// console.log(sum.add(10, 20));
+console.log(add(10, 20));
