5ec20a97480a810f60bf1a365aea553905a5c0be
91song4 <91song4@daum.net>
2022-12-12 12:33:24 +0900

    비동기 함수의 return값 확인

--- song4/nodejs/test.js
+++ song4/nodejs/test.js
@@ -1,14 +1,10 @@
 const main = async function() {
-    const countPromise = Promise.resolve(0);
-
-    function increment(val) {
-        return val + 1;
-    }
-    
-    let resultPromise;
-    console.log(resultPromise);
-    resultPromise = countPromise.then(increment).then(increment);
-    console.log(countPromise);
-    console.log(resultPromise);
-    resultPromise.then(console.log);
+    // 비동기 함수의 결과 값은 항상 promise 객체로 resolve 된다 라고 소개한다.
+    // 확인해보자.
+    console.log('test');
+    return 'test resolve';
 }();
+
+main.then(console.log);
+
+// 진짜네.. ㄷㄷ
