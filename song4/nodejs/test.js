130c2f9a9bfce73b7c7a7d67a5b0749331e1824a
91song4 <91song4@daum.net>
2022-12-12 11:31:14 +0900

    rejent를 부르기 전 에러를 만난다면 에러를 잡지 못한다.

--- song4/nodejs/test.js
+++ song4/nodejs/test.js
@@ -1,8 +1,14 @@
 const main = async function() {
     const resolvePromise = new Promise((resolve, rejent) => {
         setTimeout(() => {
-            console.log('First');
-            resolve('Resolve!');
+            console.log(First);
+            rejent('Error!!');
+
+            // try {
+            //     console.log(First);
+            // } catch {
+            //     rejent('Error!!');
+            // }
         }, 1000);
     });
 
@@ -10,5 +16,7 @@ const main = async function() {
         console.log('Middle');
         console.log('Last');
         console.log(data);
+    }).catch((err) => {
+        console.log('에러 발생!', err)
     })
 }();
