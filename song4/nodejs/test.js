1df7f8d8c1843563836c1f9ab738bc650b4960a7
91song4 <91song4@daum.net>
2022-12-12 14:27:52 +0900

    error handling 해보기

--- song4/nodejs/test.js
+++ song4/nodejs/test.js
@@ -1,15 +1,11 @@
-function setTimeoutFunc(time) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            console.log(`${time}ms에 해당하는 시간이 지났습니다.`);
-            resolve();
-        },time)
-    })
-}
-
 const main = async function () {
-    console.log('start');
-    setTimeoutFunc(2000);
-    await setTimeoutFunc(1000);
-    console.log('end');
+    const users = ['Lee', 'Kim', 'Park', 2];
+
+    try {
+        for (const user of users) {
+            console.log(user.toUpperCase());
+        }
+    } catch (err) {
+        console.error(`Error: ${err}`);
+    }
 }();
