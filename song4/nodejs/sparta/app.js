dbd0c9ff791081e26970a359695454b71a469c4a
91song4 <91song4@daum.net>
2022-12-13 12:13:18 +0900

    req.query 사용해보기

--- song4/nodejs/sparta/app.js
+++ song4/nodejs/sparta/app.js
@@ -4,12 +4,21 @@ const app = express();              // 패키지를 실행하고, app 객체를 
 const port = 3000;
 const router = require('./routes/goods.js');
 
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    console.log(req.query);
+    res.send('response');
+})
+
+
+
 // app.get으로 api를 사용가능하게 만든다.
 // app.get('/', (req, res) => {
 //     res.send('Hello World!');
 // })
 
-app.use('/', router);
+// app.use('/', router);
 
 // app.listen을 통해서 서버를 연다.
 app.listen(port, () => {
