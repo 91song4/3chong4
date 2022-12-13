672a501195e12370f152b79bfb1f8ba77c4b8f70
91song4 <91song4@daum.net>
2022-12-13 23:26:30 +0900

    mongdb에 저장해보기, 오류만남 mongoose.set(strictQuery, false);와 127.0.0.1로 해결

--- song4/nodejs/sparta/app.js
+++ song4/nodejs/sparta/app.js
@@ -1,34 +1,13 @@
-const express = require('express'); // 패키지를 들고와서 변수에 담는다.
-const app = express();              // 패키지를 실행하고, app 객체를 만든다.
-// const app = require('express')();
-const port = 3000;
+const express = require('express');
+const connect = require("./schemas");
 const router = require('./routes/goods.js');
+const app = express();
+const port = 3000;
+connect();
 
 app.use(express.json());
 
-// app.post('/', (req, res) => {
-//     console.log(req.body);
-
-//     res.send('"/"POST RUN');
-// })
-
-// app.get('/', (req, res) => {
-//     console.log(req.query);
-    
-//     res.status(400).json({'msg':'success'});
-// })
-
-// app.get('/:id', (req, res) => {
-//     console.log(req.params);
-//     res.send(':id URI response');
-// })
-
-// app.get으로 api를 사용가능하게 만든다.
-// app.get('/', (req, res) => {
-//     res.send('Hello World!');
-// })
-
-app.use('/', router);
+app.use('/api', [router]);
 
 // app.listen을 통해서 서버를 연다.
 app.listen(port, () => {
--- song4/nodejs/sparta/routes/goods.js
+++ song4/nodejs/sparta/routes/goods.js
@@ -1,60 +1,29 @@
 const express = require('express');
 const router = express.Router();
+const Goods = require("../schemas/goods");
 
-const goods = [
-    {
-        goodsId: 4,
-        name: "상품 4",
-        thumbnailUrl:
-            "https://cdn.pixabay.com/photo/2016/09/07/02/11/frogs-1650657_1280.jpg",
-        category: "drink",
-        price: 0.1,
-    },
-    {
-        goodsId: 3,
-        name: "상품 3",
-        thumbnailUrl:
-            "https://cdn.pixabay.com/photo/2016/09/07/02/12/frogs-1650658_1280.jpg",
-        category: "drink",
-        price: 2.2,
-    },
-    {
-        goodsId: 2,
-        name: "상품 2",
-        thumbnailUrl:
-            "https://cdn.pixabay.com/photo/2014/08/26/19/19/wine-428316_1280.jpg",
-        category: "drink",
-        price: 0.11,
-    },
-    {
-        goodsId: 1,
-        name: "상품 1",
-        thumbnailUrl:
-            "https://cdn.pixabay.com/photo/2016/09/07/19/54/wines-1652455_1280.jpg",
-        category: "drink",
-        price: 6.2,
-    },
-];
+router.post("/goods", async (req, res) => {
+    const { goodsId, name, thumbnailUrl, category, price } = req.body;
+    const goods = await Goods.find({ goodsId });
+    if (goods.length) {
+        return res.status(400).json({ success: false, errorMessage: "이미 있는 데이터입니다." });
+    }
 
-router.get('/goods', (req, res) => {
-    res.status(200).json({ goods });
-})
+    const createdGoods = await Goods.create({ goodsId, name, thumbnailUrl, category, price });
 
-router.get('/goods/:goodsId', (req, res) => {
-    const {goodsId} = req.params;
-    // let result;
-    // for (const good of goods) {
-    //     if (Number(goodsId) === good.goodsId) {
-    //         result = good;
-    //     }    
-    // }
+    res.json({ goods: createdGoods });
+});
 
-    const [result] = goods.filter((good) => {
-        return Number(goodsId) === good.goodsId
-    })
+//상품 목록 조회 API
+// router.get("/goods", (req, res) => {
+//     res.json({ goods: goods });
+// });
 
-
-    res.status(200).json({ "detail": result });
-})
+//상품 상세 조회 API
+// router.get("/goods/:goodsId", (req, res) => {
+//     const { goodsId } = req.params;
+//     const [detail] = goods.filter((goods) => goods.goodsId === Number(goodsId));
+//     res.json({ detail });
+// });
 
 module.exports = router;
