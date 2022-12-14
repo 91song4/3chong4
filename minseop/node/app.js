67e197783f7ebd06795be1b22db302701f22c27c
MinSeop <snsg3341@gmail.com>
2022-12-14 09:11:09 +0900

    12-14 start

--- minseop/node/app.js
+++ minseop/node/app.js
@@ -1,14 +1,21 @@
 const express = require('express')
 const app = express()
 const port = 3000
+
 const goodsRouter = require('./routes/goods')
+const cartsRouter = require('./routes/carts.js')
+const connect = require("./schemas");
+connect();
+
+
             //리퀘스트 = 요청   , 리스폰스 = 응답
 // app.get('/',(req,res) => {
 //   res.send("hello world")
 // })
+app.use(express.json())
+app.use("/api", [goodsRouter, cartsRouter])
 
-// localhost:3000/api -> goodsRouter
-app.use("/api", goodsRouter)
+
 
 app.listen(port,() => {
   console.log(port, '포트로 서버가 열렸어요')
--- minseop/node/routes/goods.js
+++ minseop/node/routes/goods.js
@@ -4,24 +4,97 @@ const express = require("express")
 //express.Router라는 함수를 실행해서 router라는 변수에 할당
 const router = express.Router();
 
-//반환 받은 router라는 변수를 이용해서
-//GET 메서드 라는걸 들어왔을때 / 기본경로로 들어왔을때 해당코드를 실행할꺼다
-//어떤코드를 반환하냐면(response) "default ~~"라는 문자열을 반환할
-//API를 하나 만들거고
+// /routes/goods.js
+const goods = [
+  {
+    goodsId: 4,
+    name: "상품 4",
+    thumbnailUrl:
+      "https://cdn.pixabay.com/photo/2016/09/07/02/11/frogs-1650657_1280.jpg",
+    category: "drink",
+    price: 0.1,
+  },
+  {
+    goodsId: 3,
+    name: "상품 3",
+    thumbnailUrl:
+      "https://cdn.pixabay.com/photo/2016/09/07/02/12/frogs-1650658_1280.jpg",
+    category: "drink",
+    price: 2.2,
+  },
+  {
+    goodsId: 2,
+    name: "상품 2",
+    thumbnailUrl:
+      "https://cdn.pixabay.com/photo/2014/08/26/19/19/wine-428316_1280.jpg",
+    category: "drink",
+    price: 0.11,
+  },
+  {
+    goodsId: 1,
+    name: "상품 1",
+    thumbnailUrl:
+      "https://cdn.pixabay.com/photo/2016/09/07/19/54/wines-1652455_1280.jpg",
+    category: "drink",
+    price: 6.2,
+  },
+];
 
-// localhost:3000/api/
-router.get('/',(req,res) =>{
-    res.send("default url for goods.js GET Method")
+// 상품 목록 조회 API
+router.get('/goods', (req,res) => {
+    res.status(200).json({"goods":goods})
 })
 
-//GET 메서드로 들어온 즉, about라는 경로에 들어온 GET메서드를 실행을 할 거다
-//res 우리는 반환을 할 거고 데이터는"goods.js ~~"문자열을 반환 할거다
-//라는 라우터를 만들었다
-
-// localhost:3000/api/about GET
-router.get('/about',(req,res) => {
-    res.send("goods.js about PATH")
+// 상품 상세 조회 API
+router.get("/goods/:goodsId", (req, res)=> {
+    const { goodsId } = req.params
+    const [result] = goods.filter((good) => Number(goodsId) === good.goodsId)
+    res.status(200).json({ "detail": result})
 })
 
+const Goods = require("../schemas/goods.js")
+router.post('/goods',async (req,res) => {
+  const {goodsId, name, thumbnailUrl, category, price} = req.body
+
+  const goods = await Goods.find({goodsId})
+
+  if (goods.length){
+    return res.status(400).json({
+      success : false,
+      errorMessage : "이미 존재하는 GoodsId입니다"
+    })
+  }
+
+  const createdGoods = await Goods.create({goodsId, name, thumbnailUrl, category, price})
+  res.json({goods : createdGoods})
+
+})
 // router 라는 변수를 밖으로 내보내 준다
 module.exports = router;
+
+
+
+
+
+
+
+
+// //반환 받은 router라는 변수를 이용해서
+// //GET 메서드 라는걸 들어왔을때 / 기본경로로 들어왔을때 해당코드를 실행할꺼다
+// //어떤코드를 반환하냐면(response) "default ~~"라는 문자열을 반환할
+// //API를 하나 만들거고
+//
+// // localhost:3000/api/
+// router.get('/',(req,res) =>{
+//     res.send("default url for goods.js GET Method")
+// })
+//
+// //GET 메서드로 들어온 즉, about라는 경로에 들어온 GET메서드를 실행을 할 거다
+// //res 우리는 반환을 할 거고 데이터는"goods.js ~~"문자열을 반환 할거다
+// //라는 라우터를 만들었다
+//
+// // localhost:3000/api/about GET
+// router.get('/about',(req,res) => {
+//     res.send("goods.js about PATH")
+// })
+
