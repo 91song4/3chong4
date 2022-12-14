3f291b62b6799d92b702621cf5e9fd6828d316e4
91song4 <91song4@daum.net>
2022-12-14 14:17:02 +0900

    스파르타 알고리즘 수요일

--- song4/nodejs/sparta/routes/goods.js
+++ song4/nodejs/sparta/routes/goods.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
-
 const Goods = require("../schemas/goods");
+const Cart = require("../schemas/cart.js");
+
+
 router.post("/goods", async (req, res) => {
     const { goodsId, name, thumbnailUrl, category, price } = req.body;
     const goods = await Goods.find({ goodsId });
@@ -14,7 +16,7 @@ router.post("/goods", async (req, res) => {
     res.json({ goods: createdGoods });
 });
 
-const Cart = require("../schemas/cart.js");
+
 router.post('/goods/:goodsId/cart', async (req, res) => {
     const { goodsId } = req.params;
     console.log(typeof (goodsId));
@@ -22,9 +24,9 @@ router.post('/goods/:goodsId/cart', async (req, res) => {
 
     const existsCarts = await Cart.find({ goodsId });
     console.log('test here');
-    console.log(typeof (existsCarts[0]['goodsId']));
+    // console.log(typeof (existsCarts[0]['goodsId']));
     if (existsCarts.length) {
-        
+
         return res.status(400).json({
             success: false,
             errorMessage: "이미 장바구니에 해당하는 상품이 존재합니다.",
@@ -35,16 +37,29 @@ router.post('/goods/:goodsId/cart', async (req, res) => {
     res.json({ result: "Success" });
 })
 
-//상품 목록 조회 API
-// router.get("/goods", (req, res) => {
-//     res.json({ goods: goods });
-// });
 
-//상품 상세 조회 API
-// router.get("/goods/:goodsId", (req, res) => {
-//     const { goodsId } = req.params;
-//     const [detail] = goods.filter((goods) => goods.goodsId === Number(goodsId));
-//     res.json({ detail });
-// });
+router.put('/goods/:goodsId/cart', async (req, res) => {
+    const { goodsId } = req.params;
+    const { quantity } = req.body;
+
+    if ((await Cart.find({ goodsId })).length) {
+        await Cart.updateOne(
+            { goodsId },
+            { $set: { quantity } }
+        );
+    }
+
+    res.status(200).json({ Success: true });
+})
+
+
+router.delete('/goods/:goodsId/cart', async (req, res) => {
+    const { goodsId } = req.params;
+    if ((await Cart.find({ goodsId })).length) {
+        await Cart.deleteOne({ goodsId });
+    }
+
+    res.status(200).json({ Success: true });
+})
 
 module.exports = router;
