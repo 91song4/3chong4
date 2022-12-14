36f8f0cc2ebc8f1b577491d6997d4dcfa136c260
Seop0728 <ms_sh0728@naver.com>
2022-12-15 00:52:32 +0900

    숙제_04

--- minseop/node/routes/post.js
+++ minseop/node/routes/post.js
@@ -1,14 +1,42 @@
 const express = require("express")
 const router = express.Router();
 
-// localhost:3000/post/
-router.get('/', (req,res) => {
-    res.send("res.send - post.js GET Method")
+// // localhost:3000/post/      예시자료
+// router.get('/', (req,res) => {
+//     res.send("res.send - post.js GET Method")
+// })
+//
+// // localhost:3000/post/about 예시자료
+// router.get('/about', (req,res) => {
+//     res.send("res.send - post.js GET Method - /about")
+// })
+
+    const data = [
+        {
+            postId: "62d6d12cd88cadd496a9e54e",
+            user: "Developer",
+            title: "안녕하세요",
+            createdAt: "2022-07-19T15:43:40.266Z"
+        },
+        {
+            postId: "62d6cc66e28b7aff02e82954",
+            user: "Developer",
+            title: "안녕하세요",
+            createdAt: "2022-07-19T15:23:18.433Z"
+        }
+    ]
+
+router.get("/", (req, res) => {
+    res.status(200).json({"data": data})
 })
 
-// localhost:3000/post/about
-router.get('/about', (req,res) => {
-    res.send("res.send - post.js GET Method - /about")
+router.get('/:postsId', (req, res) => {
+    const {postsId} = req.params
+
+    const [result] = data.filter((post) => post.postId === postsId)
+    console.log(result)
+        
+    res.status(200).json({detail: result})
 })
 
 module.exports = router
