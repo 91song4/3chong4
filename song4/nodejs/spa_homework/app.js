c214878cb03a48cd1831a657027fb7f00c22e8fc
91song4 <91song4@daum.net>
2022-12-14 18:26:45 +0900

    router 연결

--- song4/nodejs/spa_homework/app.js
+++ song4/nodejs/spa_homework/app.js
@@ -1,7 +1,12 @@
 const express = require('express');
-const connect = require('./schemas/');
+const connect = require('./schemas');
+const router = require('./schemas');
 const app = express();
 connect();
+
+app.use(express.json());
+
+app.use('/', [router]);
 const port = 3000;
 
 app.listen(port, () => {
