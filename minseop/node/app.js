1deff8439d13188c8a41ee1dfe2ffebfeef6f37e
Seop0728 <ms_sh0728@naver.com>
2022-12-14 17:06:36 +0900

    숙제_03

--- minseop/node/app.js
+++ minseop/node/app.js
@@ -4,12 +4,29 @@ const port = 3000
 
 const postRouter = require('./routes/post.js')
 
-app.get('/', (req,res) => {
-    res.send('첫페이지 입니다')
+app.use(express.json())
+
+app.post('/', (req, res) => {
+    console.log(req.body)
+    res.send('req.body 실행')
 })
 
+app.get("/", (req, res) => {
+    console.log(req.query)
+    res.send('req.query 실행')
+})
+
+app.get('/:id', (req, res) => {
+    console.log(req.params)
+    res.send(':id / req.params 실행')
+})
+
+// app.get('/', (req,res) => {
+//     res.send('첫페이지 입니다')
+// })
+
 // 전역 미들웨어
-app.use("/post", postRouter)
+// app.use("/post", postRouter)
 
 app.listen(port, () => {
     console.log(port, '포트로 서버를 열었어요( app.listen )')
