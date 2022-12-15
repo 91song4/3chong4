179b65f5483d7417546fb7919c1b83bd02790aa4
91song4 <91song4@daum.net>
2022-12-16 02:57:08 +0900

    GET에 대한 모든 요청 발견!

--- song4/nodejs/spa_homework/app.js
+++ song4/nodejs/spa_homework/app.js
@@ -7,19 +7,37 @@ const app = express();
 const port = 3000;
 connect();
 
-app.use(express.json());
-console.log('app.js test here');
-// app.use('/',mongoose.Types.ObjectId);
-app.use((req,res, next) =>
+app.get('/*',(req, res,next) =>
 {
-    console.log('app.js : app.get() test here');
+    console.log('test1');
     next();
-});
+})
 
-// req를 인터셉트해서 다시 다음 동작이 하도록 보내주기.
+app.use((req, res, next) =>
+{
+    console.log('test2');
+    next();
+})
 
-app.use('/posts', postRouter);
-app.use('/comments', commentRouter);
+
+
+
+
+
+// app.use(express.json());
+// console.log('app.js test here');
+// // app.use('/',mongoose.Types.ObjectId);
+// app.use((req,res, next) =>
+// {
+//     console.log('app.js : app.get() test here');
+//     mongoose.Types.ObjectId;
+//     next();
+// });
+
+// // req를 인터셉트해서 다시 다음 동작이 하도록 보내주기.
+
+// app.use('/posts', postRouter);
+// app.use('/comments', commentRouter);
 
 app.listen(port, () => {
     console.log(`Server On Port: ${port}`);
