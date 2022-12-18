be9c6d2560447b540d461279535cb114b1a3f7e7
91song4 <91song4@daum.net>
2022-12-19 00:59:25 +0900

    automatic semicolon insetion이 모든 곳에 적용하지 않는다. 대괄호가 등장하면 ([])앞에 세미콜론이 있다고 가정하지 않는다.

--- song4/javascript/homework.js
+++ song4/javascript/homework.js
@@ -1,2 +1,4 @@
 console.log('자바스크립트!') //줄바꿈시 세미콜론 자동 삽입 (automatic semicolon insetion)
-console.log('automatic semicolon insertion');
+console.log('automatic semicolon insertion')
+
+[1, 2].forEach(console.log)
