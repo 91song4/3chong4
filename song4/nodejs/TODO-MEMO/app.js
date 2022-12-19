9f345482bb49a0f1a2f94f7533ce37abbb9b5d7b
91song4 <91song4@daum.net>
2022-12-19 22:16:41 +0900

    session 발급과 조회

--- song4/nodejs/TODO-MEMO/app.js
+++ song4/nodejs/TODO-MEMO/app.js
@@ -1,45 +1,30 @@
 const express = require("express");
-const cookieParser = require("cookie-parser");
+const cookieParser = require('cookie-parser');
 
 const db = require('./models/index.js');
 const todosRouter = require("./routes/todos.router.js");
 
 const app = express();
-
 app.use(cookieParser());
+let session = {};
 
 app.use("/api", express.json(), todosRouter);
 app.use(express.static("./assets"));
 
-// app.get('/set-cookie', (req, res) => {
-//     console.log('set1');
-//     const expire = new Date();
-//     expire.setMinutes(expire.getMinutes() + 60); // 쿠키만료시간 60분 설정.
+app.get('/set-session', (req, res, next) => {
+    const name = 'sparta';
+    const uniqueInt = Date.now();
+    session[uniqueInt] = { name };
 
-//     res.writeHead(200, {
-//         'set-Cookie': `name=sparta; Expries=${expire.toGMTString()}; HttpOnly; Path=/`,
-//     });
-//     return res.status(200).end();
-// })
-
-app.get('/set-cookie', (req, res) => {
-    console.log('set2');
-    const expires = new Date();
-    expires.setMinutes(expires.getMinutes() + 60);
-
-    res.cookie('name', 'sparta', {
-        expires: expires
-    });
+    res.cookie('sessionKey', uniqueInt);
     return res.status(200).end();
 })
 
-app.get('/get-cookie', (req, res) => {
-    const cookie = req.cookies;
-    console.log(cookie);
-    return res.status(200).json({ cookie });
-})
-
-
+app.get('/get-session', (req, res) => {
+    const { sessionKey } = req.cookies;
+    const name = session[sessionKey];
+    return res.status(200).json({ name });
+})    
 
 
 app.listen(8080, () => {
