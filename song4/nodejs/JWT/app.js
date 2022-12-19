ebb2556552641cf2d66ded54db8ee108b255a864
91song4 <91song4@daum.net>
2022-12-19 22:38:14 +0900

    verify JWT

--- song4/nodejs/JWT/app.js
+++ song4/nodejs/JWT/app.js
@@ -3,9 +3,9 @@ const jwt = require('jsonwebtoken');
 const payload = {
     "myPayloadData": 1234,
 }
+const secretKey = "mysecretkey";
 
 // Create JWT
-const secretKey = "mysecretkey";
 const token = jwt.sign(
     payload,
     secretKey
@@ -13,5 +13,11 @@ const token = jwt.sign(
 // console.log(token);
 
 // JWT decoded
+// jwt의 Payload를 확인하기 위해서 사용한다.
 const decodeToken = jwt.decode(token);
-console.log(decodeToken);
+// console.log(decodeToken);
+
+// verify JWT
+const verifyToken = jwt.verify(token, secretKey);
+// const verifyToken = jwt.verify(token, "secretKey");
+console.log(verifyToken);
