d68157e6ae0da682ca645f7430064fad06792aa6
Seop0728 <ms_sh0728@naver.com>
2022-12-19 15:40:44 +0900

    조회,순서변경

--- minseop/node/app.js
+++ minseop/node/app.js
@@ -1,36 +1,13 @@
-const express = require('express')  // require
-const app = express()
-const port = 3000
+const express = require("express");
 
-const postRouter = require('./routes/posts.js')
-const commentRouter = require('./routes/comments.js')
-const connect = require('./schemas/')
-connect()
+const db = require("./models/index.js")
+const todosRouter = require("./routes/todos.router.js");
 
-app.use(express.json());    // 바디에 데이터가 들어왔을때 사용할 수 있도록 만들어준답
-app.use("/posts", [postRouter,commentRouter])
+const app = express();
 
-app.get('/', (req,res) => {
-    res.send('첫페이지 입니다')
-})
+app.use("/api", express.json(), todosRouter);
+app.use(express.static("./assets"));
 
-app.listen(port, () => {
-    console.log(port, '포트로 서버를 열었어요( app.listen )')
-})
-
-
-// app.post('/', (req, res) => {
-//     console.log(req.body)
-//     res.send('req.body 실행')
-// })
-//
-// app.get("/", (req, res) => {
-//     console.log(req.query)
-//     res.send('req.query 실행')
-// })
-//
-// app.get('/:id', (req, res) => {
-//     console.log(req.params)
-//     res.send(':id / req.params 실행')
-// })
-
+app.listen(8080, () => {
+  console.log("서버가 켜졌어요!");
+});
