c696c5cc63ab9f28a0639dab415bb0d6cff4776b
Seop0728 <ms_sh0728@naver.com>
2022-12-28 02:35:56 +0900

    node 연습

--- minseop/min_run.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, render_template, request, jsonify
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return render_template('min_index.html')
-
-if __name__ == '__main__':
-    app.run()
