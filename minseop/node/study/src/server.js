a1f88fe0d5c988c968c4970a1e0249acd9aeb37c
Seop0728 <ms_sh0728@naver.com>
2022-12-29 20:55:44 +0900

    feat : node_study

--- minseop/node/study/src/server.js
+++ minseop/node/study/src/server.js
@@ -1,7 +1,16 @@
 import express from "express";
+import path from 'path';
+const __dirname = path.resolve();
 
 const app = express();
+app.set("view engine", "pug");
+app.set("views", __dirname + "/src/views");
 
-console.log('hello');
+app.use("/public", express.static(__dirname + "/src/public"));
 
-app.listen(3000);
+app.get('/', (req,res) => res.render("home"))
+app.get('/*', (req, res) => res.redirect('/'))
+
+app.listen(3000, () => {
+  console.log('서버가 요청을 받을 준비가 됐어요');
+});
