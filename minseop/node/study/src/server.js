a09a8a751e8cf9a81842391ccb740e74bf4ae36c
Seop0728 <ms_sh0728@naver.com>
2023-01-09 16:20:24 +0900

    add socket

--- minseop/node/study/src/server.js
+++ minseop/node/study/src/server.js
@@ -1,16 +1,29 @@
 import express from "express";
 import path from 'path';
+import http from "http"
+import WebSocket, { WebSocketServer } from "ws";
+
 const __dirname = path.resolve();
 
 const app = express();
+
 app.set("view engine", "pug");
 app.set("views", __dirname + "/src/views");
 
 app.use("/public", express.static(__dirname + "/src/public"));
 
-app.get('/', (req,res) => res.render("home"))
-app.get('/*', (req, res) => res.redirect('/'))
+app.get('/', ( req, res ) => res.render("home"));
+app.get('/*', ( req, res ) => res.redirect('/'));
 
-app.listen(3000, () => {
-  console.log('서버가 요청을 받을 준비가 됐어요');
-});
+const handleListen = () =>console.log(`listening on http://localhost:3000`)
+
+const server = http.createServer(app)
+const wss = new WebSocketServer ({server })
+
+function handleConnection(socket) {
+	console.log(socket);
+}
+
+wss.on("connection", handleConnection)
+
+server.listen(3000, handleListen);
