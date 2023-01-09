cd07fc4e9e29030236947763e7dcf440681ebfec
Seop0728 <ms_sh0728@naver.com>
2023-01-09 17:04:00 +0900

    socket send Message

--- minseop/node/study/src/public/js/app.js
+++ minseop/node/study/src/public/js/app.js
@@ -1,1 +1,17 @@
 const socket = new WebSocket(`ws://${window.location.host}`)
+
+socket.addEventListener("open", ()=> {
+	console.log("Connected to Sever ✅ ");
+})
+
+socket.addEventListener("message", (message) => {
+	console.log("New message : ",message.data ,"from the Server");
+})
+
+socket.addEventListener("close", ()=>{
+	console.log("DisConnected to Sever ❌ ");
+})
+
+setTimeout(()=> {
+	socket.send("hello from the browser!!");
+},10000)
--- minseop/node/study/src/server.js
+++ minseop/node/study/src/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import path from 'path';
-import http from "http"
+import http from "http";
 import WebSocket, { WebSocketServer } from "ws";
 
 const __dirname = path.resolve();
@@ -15,15 +15,19 @@ app.use("/public", express.static(__dirname + "/src/public"));
 app.get('/', ( req, res ) => res.render("home"));
 app.get('/*', ( req, res ) => res.redirect('/'));
 
-const handleListen = () =>console.log(`listening on http://localhost:3000`)
+const handleListen = () => console.log(`listening on http://localhost:3000`);
 
-const server = http.createServer(app)
-const wss = new WebSocketServer ({server })
+const server = http.createServer(app);
+const wss = new WebSocketServer({ server });
 
-function handleConnection(socket) {
-	console.log(socket);
-}
 
-wss.on("connection", handleConnection)
+wss.on("connection", ( socket ) => {
+	console.log("Connected to Browser ✅ ");
+	socket.on("close", () => console.log("DisConnected from the Browser ❌ "));
+	socket.on("message", (message)=> {
+		console.log(message.toString('utf8'));
+	})
+	socket.send("hello~~");
+});
 
 server.listen(3000, handleListen);
