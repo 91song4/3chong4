bbb69395fb2a5e59ffaa05ea099e7a7f09f686e7
91song4@daum.net <91song4@daum.net>
2023-01-11 04:22:51 +0900

    알고리즘: 나누어 떨어지는 숫자 배열

---
+++ song4/algorithm/quiz/spa_algo_230103.js
@@ -0,0 +1,9 @@
+// 나누어 떨어지는 숫자 배열
+
+function solution(arr, divisor) {
+  let answer = arr.filter((num) => num % divisor === 0);
+
+  if (answer.length === 0)
+    return [-1]
+  return answer.sort((a, b) => a - b);
+}
