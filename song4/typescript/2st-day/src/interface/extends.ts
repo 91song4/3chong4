28a9e2034ff280e5b2fe705469f457a3e9107f1e
91song4 <91song4@daum.net>
2023-01-18 01:03:01 +0900

    TypeScript-fix: 인터페이스 다중상속 예제 추가

--- song4/typescript/2st-day/src/interface/extends.ts
+++ song4/typescript/2st-day/src/interface/extends.ts
@@ -2,12 +2,19 @@ interface Person {
   name: string;
 }
 
-interface Developer extends Person {
+interface Drinker {
+  drink: string;
+}
+
+interface Developer extends Person, Drinker {
   skill: string;
 }
 
 let fe = {} as Developer;
 // let fe: Developer = { name: "song", skill: "passion" };
 
-fe.name = "josh";
-fe.skill = "TypeScript";
+fe.name = "song";
+fe.skill = "PASSION";
+fe.drink = "MonsterBlack";
+
+console.log(fe);
