8501901d27aec12baf2be04b3e4533d667f78821
91song4 <91song4@daum.net>
2023-01-17 19:39:56 +0900

    TypeScript-feat: array type

---
+++ song4/typescript/2st-day/src/array.ts
@@ -0,0 +1,11 @@
+let nList: number[] = [1, 2, 3];
+let szList: string[] = ['안', '녕', '하', '세', '요'];
+
+//제너릭 배열타입
+let nListGeneric: Array<number> = [1, 2, 3];
+let szListGeneric: Array<string> = ["안", "녕", "하", "세", "요"];
+
+console.log('nList', nList);
+console.log('szList', szList);
+console.log('nListGeneric', nListGeneric);
+console.log('szListGeneric', szListGeneric);
