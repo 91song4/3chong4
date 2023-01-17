384184ff6b81e5a4a3afecc861abf633ee7b5f93
91song4 <91song4@daum.net>
2023-01-17 19:13:32 +0900

    TypeScript-feat: number type

---
+++ song4/typescript/2st-day/src/number.ts
@@ -0,0 +1,9 @@
+let decimal: number = 6;      // 10진수
+let hex: number = 0xf00d;     // 16진수
+let binary: number = 0b1010;  // 2진수
+let octal: number = 0o744;    // 8진수
+
+console.log('decimal', decimal);
+console.log('hex', hex);
+console.log('binary', binary);
+console.log('octal', octal);
