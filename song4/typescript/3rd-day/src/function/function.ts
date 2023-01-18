561d967930d7e223b26d5d42f58700f4ffedfbb8
91song4 <91song4@daum.net>
2023-01-18 19:23:21 +0900

    fix: 에러 주석처리

--- song4/typescript/3rd-day/src/function/function.ts
+++ song4/typescript/3rd-day/src/function/function.ts
@@ -3,9 +3,9 @@
  * tsconfig.json 에서 "noImplicitAny": true, 옵션을 줘서 암시적 any타입을 허용하지 않는다.
  * 그래서 이 함수는 에러가 난다.
  */
-function function_sum1(a, b) {
-  return a + b;
-}
+// function function_sum1(a, b) {
+//   return a + b;
+// }
 
 function function_sum2(a: number, b: number): number {
   return a + b;
