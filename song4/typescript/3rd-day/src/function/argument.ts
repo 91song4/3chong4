1d66393ad6b34a6510646e27e24b731c219f4900
91song4 <91song4@daum.net>
2023-01-18 19:10:31 +0900

    TypeScript-feat: optional parameters for function

---
+++ song4/typescript/3rd-day/src/function/argument.ts
@@ -0,0 +1,21 @@
+function argument_sum(a: number, b: number): number {
+  return a + b;
+}
+
+argument_sum(10, 20)        // a + b
+argument_sum(10, 20, 30)    // err, too many parameters
+argument_sum(10)            // err, too few parameters
+
+
+function argument_sum2(a: number, b?: number): number {
+  if (b === undefined)
+    return a;
+
+  return a + b;
+}
+
+argument_sum2(10, 20)        // a + b
+argument_sum2(10, 20, 30)    // err, too many parameters
+argument_sum2(10)            // a | a + b
+
+export { argument_sum, argument_sum2 };
