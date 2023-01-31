61735dd67b3745abae21855ad0ba86187cea8acd
gyuryeol jeon <wjsrbfuf@gmail.com>
2023-01-31 11:50:57 +0900

    typescript generic types

--- ggu/typescript/typescript.nosync/src/index.ts
+++ ggu/typescript/typescript.nosync/src/index.ts
@@ -1,3 +1,46 @@
+interface IStack<T> {
+  push(item: T): void;
+  pop(): T | undefined;
+  peek(): T | undefined;
+  size(): number;
+}
+
+class Stack<T> implements IStack<T> {
+  // Queue
+  private storage: T[] = [];
+
+  constructor(private capacity = 4) {}
+
+  push(item: T): void {
+    if (this.size() === this.capacity) {
+      throw Error("stack is full");
+    }
+
+    this.storage.push(item);
+  }
+
+  pop(): T | undefined {
+    return this.storage.pop();
+  }
+
+  peek(): T | undefined {
+    return this.storage[this.size() - 1];
+  }
+
+  size(): number {
+    return this.storage.length;
+  }
+}
+
+const stringStack = new Stack<string>();
+stringStack.push("hello");
+stringStack.push("world");
+stringStack.push("!");
+stringStack.push("!");
+stringStack.push("!");
+
+console.log(stringStack.peek());
+
 // interface TableData {
 //   [x: string]: string;
 // }
@@ -19,19 +62,3 @@
 // };
 
 // console.log(myTableData);
-
-import { Color } from "./types"
-
-const { Red, Green, Blue: ImBlue } = Color;
-
-const green = Color.Green;
-
-const veggies = Color.Green;
-
-const pepper = Color.Red;
-
-const sky = ImBlue;
-
-console.log(sky)
-
-// DRY (Don't Repeat Yourself) => 개발 원칙
--- ggu/typescript/typescript.nosync/src/types/index.ts
+++ ggu/typescript/typescript.nosync/src/types/index.ts
@@ -1,22 +1,49 @@
+import exp from "constants";
+
 export enum Color {
-    RED = "RED",
-    Green = "GREEN",
-    BLUE = "BLUE"
+  RED = "RED",
+  Green = "GREEN",
+  BLUE = "BLUE",
 }
 
 export enum Direction {
-    Up = "Up",
-    Down = "Down",
-    Left = "Left",
-    Right = "Right",
-  }
-  
-  // type TThingsInLife = {
-  //     colorOfPen: Color;
-  //     keyboardArrow: Direction;
-  // }
-  
-  export interface ThingsInLife {
-    colorOfPen: Color;
-    keyboardArrow: Direction;
-  }
+  Up = "Up",
+  Down = "Down",
+  Left = "Left",
+  Right = "Right",
+}
+
+// type TThingsInLife = {
+//     colorOfPen: Color;
+//     keyboardArrow: Direction;
+// }
+
+export interface ThingsInLife {
+  colorOfPen: Color;
+  keyboardArrow: Direction;
+}
+
+export interface MyInterface {
+  value: string | number | string[] | number[] | boolean | boolean[];
+}
+
+export interface MyInterfaceG<T = string> {
+  value: T;
+}
+
+export type User = {
+  email: string;
+  name: string;
+}
+
+export enum Status {
+  Initiated = "Initiated",
+  Pending = "Pending",
+  Shipped = "Shipped",
+  Delivered = "Delivered",
+}
+
+export interface Order {
+  buyer: string;
+  orderStatus: Status;
+}
