dc6ff7f621718b84ba4b92c8828e7f511d508f12
91song4@daum.net <91song4@daum.net>
2023-02-26 00:25:07 +0900

    nestjs 복습준비

--- song4/nestjs-prac/src/board/dto/update-article.dto.ts
+++
@@ -1,9 +0,0 @@
-import { CreateArticleDto } from './create-article.dto';
-import { PartialType } from '@nestjs/mapped-types';
-
-export class UpdateArticleBodyDto extends PartialType(CreateArticleDto) {}
-
-export type UpdateArticleDto = Pick<
-  UpdateArticleBodyDto,
-  'title' | 'content' | 'password'
-> & { id: number };
--- song4/nestjs-prac/src/board/model/article.model.ts
+++
@@ -1,5 +0,0 @@
-export interface IArticle {
-  id: number;
-  title: string;
-  content: string;
-}
